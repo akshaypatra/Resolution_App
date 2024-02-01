59dc6482bc2a6e84f2f810c281406f92b832cccf
akshaypatra <akshay.patra114@gmail.com>
2024-02-01 09:23:17 +0530

    Adding Files

--- app/src/main/java/com/example/new_year_resolutions/ResolutionRepo.kt
+++ app/src/main/java/com/example/new_year_resolutions/ResolutionRepo.kt
@@ -1,16 +1,4 @@
 package com.example.new_year_resolutions
 
-val resolutions= listOf(
-    Resolution(
-        "Wake Up Early"
-    ),
-    Resolution(
-        "Attend GDSC events"
-    ),
-    Resolution(
-        "Eat healthy"
-    ),
-    Resolution(
-        "Be Happy"
-    )
-)
+
+val resolutions= listOf<Resolution>()
